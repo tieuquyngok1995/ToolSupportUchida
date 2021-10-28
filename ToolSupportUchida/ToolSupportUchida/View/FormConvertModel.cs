f7ec90ea34d2efba488c61afb72d0dc448970015
tieuquyngok1995 <tieuquyngok1995@gmail.com>
2021-10-28 17:50:16 +0700

    Update FormConvertModel.cs

--- ToolSupportUchida/ToolSupportUchida/View/FormConvertModel.cs
+++ ToolSupportUchida/ToolSupportUchida/View/FormConvertModel.cs
@@ -802,7 +802,7 @@ namespace ToolSupportUchida.View
                     }
                     else if (rdbObservable.Checked)
                     {
-                        stringBuilder.Append("readonly {0}{2}{3};\r\n");
+                        stringBuilder.Append("readonly {0}{1}{2};");
                     }
                     else
                     {
