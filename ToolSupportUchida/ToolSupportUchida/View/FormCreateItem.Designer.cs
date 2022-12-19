232c6299aff58acbf894578991a63ca3436bc0bc
Vu Quoc Tuan <tuan-vq@fuijinet.net>
2022-12-19 17:14:58 +0700

    commit

--- ToolSupportUchida/ToolSupportUchida/View/FormCreateItem.Designer.cs
+++ ToolSupportUchida/ToolSupportUchida/View/FormCreateItem.Designer.cs
@@ -52,10 +52,10 @@ namespace ToolSupportCoding.View
             this.colValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.index = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panelResult = new System.Windows.Forms.Panel();
+            this.txtResult = new System.Windows.Forms.RichTextBox();
             this.btnCreateResult = new System.Windows.Forms.Button();
             this.lblResult = new System.Windows.Forms.Label();
             this.btnCopy = new System.Windows.Forms.Button();
-            this.txtResult = new System.Windows.Forms.TextBox();
             this.panelSetting.SuspendLayout();
             this.groupBoxSetting.SuspendLayout();
             this.panelData.SuspendLayout();
@@ -86,9 +86,9 @@ namespace ToolSupportCoding.View
             this.groupBoxSetting.Controls.Add(this.cbType);
             this.groupBoxSetting.Controls.Add(this.label9);
             this.groupBoxSetting.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.groupBoxSetting.Location = new System.Drawing.Point(6, 3);
+            this.groupBoxSetting.Location = new System.Drawing.Point(9, 1);
             this.groupBoxSetting.Name = "groupBoxSetting";
-            this.groupBoxSetting.Size = new System.Drawing.Size(686, 65);
+            this.groupBoxSetting.Size = new System.Drawing.Size(682, 65);
             this.groupBoxSetting.TabIndex = 0;
             this.groupBoxSetting.TabStop = false;
             this.groupBoxSetting.Text = "Setting";
@@ -100,7 +100,7 @@ namespace ToolSupportCoding.View
             this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnReset.Image = global::ToolSupportCoding.Properties.Resources.create;
             this.btnReset.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnReset.Location = new System.Drawing.Point(602, 35);
+            this.btnReset.Location = new System.Drawing.Point(597, 34);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(78, 24);
             this.btnReset.TabIndex = 15;
@@ -117,9 +117,9 @@ namespace ToolSupportCoding.View
             this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCreate.Image = global::ToolSupportCoding.Properties.Resources.button_create_in;
             this.btnCreate.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCreate.Location = new System.Drawing.Point(469, 35);
+            this.btnCreate.Location = new System.Drawing.Point(464, 34);
             this.btnCreate.Name = "btnCreate";
-            this.btnCreate.Size = new System.Drawing.Size(128, 24);
+            this.btnCreate.Size = new System.Drawing.Size(127, 24);
             this.btnCreate.TabIndex = 9;
             this.btnCreate.Text = "  Create Param";
             this.btnCreate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -130,7 +130,7 @@ namespace ToolSupportCoding.View
             // txtFormat
             // 
             this.txtFormat.Enabled = false;
-            this.txtFormat.Location = new System.Drawing.Point(243, 35);
+            this.txtFormat.Location = new System.Drawing.Point(239, 34);
             this.txtFormat.Name = "txtFormat";
             this.txtFormat.Size = new System.Drawing.Size(110, 23);
             this.txtFormat.TabIndex = 14;
@@ -139,7 +139,7 @@ namespace ToolSupportCoding.View
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(240, 19);
+            this.label3.Location = new System.Drawing.Point(236, 17);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(95, 15);
             this.label3.TabIndex = 13;
@@ -147,7 +147,7 @@ namespace ToolSupportCoding.View
             // 
             // txtValue
             // 
-            this.txtValue.Location = new System.Drawing.Point(360, 35);
+            this.txtValue.Location = new System.Drawing.Point(355, 34);
             this.txtValue.Name = "txtValue";
             this.txtValue.Size = new System.Drawing.Size(103, 23);
             this.txtValue.TabIndex = 12;
@@ -156,7 +156,7 @@ namespace ToolSupportCoding.View
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(357, 19);
+            this.label2.Location = new System.Drawing.Point(353, 17);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 15);
             this.label2.TabIndex = 11;
@@ -167,7 +167,7 @@ namespace ToolSupportCoding.View
             this.cbKey.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbKey.FormattingEnabled = true;
-            this.cbKey.Location = new System.Drawing.Point(127, 35);
+            this.cbKey.Location = new System.Drawing.Point(123, 34);
             this.cbKey.Name = "cbKey";
             this.cbKey.Size = new System.Drawing.Size(110, 23);
             this.cbKey.TabIndex = 9;
@@ -176,7 +176,7 @@ namespace ToolSupportCoding.View
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(124, 19);
+            this.label1.Location = new System.Drawing.Point(120, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(54, 15);
             this.label1.TabIndex = 10;
@@ -187,7 +187,7 @@ namespace ToolSupportCoding.View
             this.cbType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbType.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbType.FormattingEnabled = true;
-            this.cbType.Location = new System.Drawing.Point(10, 35);
+            this.cbType.Location = new System.Drawing.Point(7, 34);
             this.cbType.Name = "cbType";
             this.cbType.Size = new System.Drawing.Size(110, 23);
             this.cbType.TabIndex = 7;
@@ -197,7 +197,7 @@ namespace ToolSupportCoding.View
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(7, 19);
+            this.label9.Location = new System.Drawing.Point(4, 17);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(60, 15);
             this.label9.TabIndex = 8;
@@ -217,9 +217,9 @@ namespace ToolSupportCoding.View
             // 
             this.groupBox1.Controls.Add(this.gridSetParam);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.groupBox1.Location = new System.Drawing.Point(6, -1);
+            this.groupBox1.Location = new System.Drawing.Point(9, -4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(686, 213);
+            this.groupBox1.Size = new System.Drawing.Size(682, 213);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Set Param ";
@@ -265,7 +265,7 @@ namespace ToolSupportCoding.View
             this.gridSetParam.RowHeadersWidth = 25;
             this.gridSetParam.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.gridSetParam.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.gridSetParam.Size = new System.Drawing.Size(680, 191);
+            this.gridSetParam.Size = new System.Drawing.Size(676, 191);
             this.gridSetParam.TabIndex = 1;
             this.gridSetParam.CurrentCellDirtyStateChanged += new System.EventHandler(this.gridSetParam_CurrentCellDirtyStateChanged);
             // 
@@ -305,15 +305,24 @@ namespace ToolSupportCoding.View
             // 
             // panelResult
             // 
+            this.panelResult.Controls.Add(this.txtResult);
             this.panelResult.Controls.Add(this.btnCreateResult);
             this.panelResult.Controls.Add(this.lblResult);
             this.panelResult.Controls.Add(this.btnCopy);
-            this.panelResult.Controls.Add(this.txtResult);
             this.panelResult.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panelResult.Location = new System.Drawing.Point(0, 216);
             this.panelResult.Name = "panelResult";
             this.panelResult.Size = new System.Drawing.Size(704, 88);
             this.panelResult.TabIndex = 1;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(9, 2);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(683, 42);
+            this.txtResult.TabIndex = 17;
+            this.txtResult.Text = "";
             // 
             // btnCreateResult
             // 
@@ -323,7 +332,7 @@ namespace ToolSupportCoding.View
             this.btnCreateResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCreateResult.Image = global::ToolSupportCoding.Properties.Resources.button_create_out;
             this.btnCreateResult.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCreateResult.Location = new System.Drawing.Point(9, 58);
+            this.btnCreateResult.Location = new System.Drawing.Point(9, 53);
             this.btnCreateResult.Name = "btnCreateResult";
             this.btnCreateResult.Size = new System.Drawing.Size(128, 24);
             this.btnCreateResult.TabIndex = 16;
@@ -338,7 +347,7 @@ namespace ToolSupportCoding.View
             this.lblResult.AutoSize = true;
             this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblResult.ForeColor = System.Drawing.Color.Red;
-            this.lblResult.Location = new System.Drawing.Point(570, 65);
+            this.lblResult.Location = new System.Drawing.Point(559, 62);
             this.lblResult.Name = "lblResult";
             this.lblResult.Size = new System.Drawing.Size(130, 13);
             this.lblResult.TabIndex = 12;
@@ -353,7 +362,7 @@ namespace ToolSupportCoding.View
             this.btnCopy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCopy.Image = global::ToolSupportCoding.Properties.Resources.button_copy_clipboar;
             this.btnCopy.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCopy.Location = new System.Drawing.Point(483, 58);
+            this.btnCopy.Location = new System.Drawing.Point(473, 53);
             this.btnCopy.Name = "btnCopy";
             this.btnCopy.Size = new System.Drawing.Size(85, 26);
             this.btnCopy.TabIndex = 10;
@@ -361,17 +370,6 @@ namespace ToolSupportCoding.View
             this.btnCopy.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnCopy.UseVisualStyleBackColor = true;
             this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
-            // 
-            // txtResult
-            // 
-            this.txtResult.Location = new System.Drawing.Point(8, 6);
-            this.txtResult.Multiline = true;
-            this.txtResult.Name = "txtResult";
-            this.txtResult.ReadOnly = true;
-            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtResult.Size = new System.Drawing.Size(684, 48);
-            this.txtResult.TabIndex = 4;
-            this.txtResult.TabStop = false;
             // 
             // FormCreateItem
             // 
@@ -412,7 +410,6 @@ namespace ToolSupportCoding.View
         private System.Windows.Forms.Button btnReset;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.DataGridView gridSetParam;
-        private System.Windows.Forms.TextBox txtResult;
         private System.Windows.Forms.Button btnCopy;
         private System.Windows.Forms.Label lblResult;
         private System.Windows.Forms.Button btnCreateResult;
@@ -420,5 +417,6 @@ namespace ToolSupportCoding.View
         private System.Windows.Forms.DataGridViewTextBoxColumn colKey;
         private System.Windows.Forms.DataGridViewTextBoxColumn colValue;
         private System.Windows.Forms.DataGridViewTextBoxColumn index;
+        private System.Windows.Forms.RichTextBox txtResult;
     }
 }
